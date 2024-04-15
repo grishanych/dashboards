ad2c8048afe8fa0cf5050c67443533adbd2d262e
Grygorii Nych <alsolo2011@gmail.com>
2024-04-15 23:28:59 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+// function changeSvgColor() {
+//     var svgs = document.getElementsByClassName("svg-change-color");
+//     for (var i = 0; i < svgs.length; i++) {
+//       svgs[i].style.stroke  = "#fff"; 
+
+//     }
+//   }
+  
+//   function resetSvgColor() {
+//     var svgs = document.getElementsByClassName("svg-change-color");
+//     for (var i = 0; i < svgs.length; i++) {
+//       svgs[i].style.stroke = "none"; // Reset to original color
+      
+//     }
+//   }
+  
